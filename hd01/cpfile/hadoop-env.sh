d27da5f938c94b044b12fa0488e6ecf0172f29d7
y0ngma <takeit2sy@pusan.ac.kr>
2023-06-01 17:39:07 +0900

    copy config files

--- hd01/cpfile/hadoop-env.sh
+++ hd01/cpfile/hadoop-env.sh
@@ -415,6 +415,11 @@ export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 #
 # For example, to limit who can execute the namenode command,
 # export HDFS_NAMENODE_USER=hdfs
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
 
 
 ###
