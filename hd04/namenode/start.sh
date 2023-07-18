8e5c2de285461f8225447acae3680b01d8093c6a
y0ngma <takeit2sy@pusan.ac.kr>
2023-07-18 17:14:07 +0900

    namenode up

--- hd04/namenode/start.sh
+++ hd04/namenode/start.sh
@@ -3,12 +3,6 @@
 ## NameNode의 네임스페이스가 포맷되었는지 확인 및 포맷진행
 # 네임스페이스 디렉토리를 입력받아서 
 NAME_DIR=$1
-# echo "$JAVA_HOME 내 파일 목록 ============================================== "
-# echo "$(ls $JAVA_HOME)"
-# echo "$HADOOP_HOME/bin/hdfs 파일 목록 ============================================== "
-# echo "$(ls $HADOOP_HOME/bin/hdfs)"
-# echo "$HADOOP_CONF_DIR 파일 목록 ============================================== "
-# echo "$(ls $HADOOP_CONF_DIR)"
 
 # 비어있지 않다면 이미 포맷된 것이므로 건너뛰고
 if [ "$(ls -A $NAME_DIR)" ]; then
